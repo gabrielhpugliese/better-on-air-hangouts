59bcf745692369518dfc754ffcbb14c794270733
Matthew Tole <matthewtole@gmail.com>
2012-02-19 11:32:07 +0000

    Changed bracket position and removed some debugging code.

--- code/betteronairhangouts.js
+++ code/betteronairhangouts.js
@@ -2,30 +2,24 @@ var HANGOUT_IMAGE_CLASS = "gt";
 
 BetterOnAirHangouts = function() {};
 
-BetterOnAirHangouts.prototype.init = function()
-{
+BetterOnAirHangouts.prototype.init = function() {
     var content_pane = document.querySelector("#contentPane");
-    if (content_pane)
-    {
+    if (content_pane) {
         content_pane.addEventListener('DOMNodeInserted', this.onGooglePlusContentModified.bind(this), false);
         setTimeout(this.renderAllItems.bind(this), 100);
     }
 };
 
-BetterOnAirHangouts.prototype.onGooglePlusContentModified = function(e)
-{
-    if (e.relatedNode && e.relatedNode.parentNode && e.relatedNode.parentNode.id == 'contentPane')
-    {
+BetterOnAirHangouts.prototype.onGooglePlusContentModified = function(e) {
+    if (e.relatedNode && e.relatedNode.parentNode && e.relatedNode.parentNode.id == 'contentPane') {
         this.renderAllItems(e.target);
     }
-    else if (e.target.nodeType == Node.ELEMENT_NODE && e.target.id.indexOf('update') == 0)
-    {
+    else if (e.target.nodeType == Node.ELEMENT_NODE && e.target.id.indexOf('update') == 0) {
         this.renderItem(e.target);
     }
 };
 
-BetterOnAirHangouts.prototype.onExpandButtonClick = function(e)
-{
+BetterOnAirHangouts.prototype.onExpandButtonClick = function(e) {
     var iframe = $("iframe", this.findPost(e.target));
     width = iframe.width();
     height = iframe.height();
@@ -33,8 +27,7 @@ BetterOnAirHangouts.prototype.onExpandButtonClick = function(e)
     iframe.height(iframe.width() * (height / width));
 };
 
-BetterOnAirHangouts.prototype.onPlayAgainClick = function(e)
-{
+BetterOnAirHangouts.prototype.onPlayAgainClick = function(e) {
     var post = this.findPost(e.target)
     var iframe = $("iframe", post);
 
@@ -46,8 +39,7 @@ BetterOnAirHangouts.prototype.onPlayAgainClick = function(e)
 
 };
 
-BetterOnAirHangouts.prototype.onNewTabButtonClick = function(e)
-{
+BetterOnAirHangouts.prototype.onNewTabButtonClick = function(e) {
     var post = this.findPost(e.target)
     var iframe = $("iframe", post);
 
@@ -67,29 +59,23 @@ BetterOnAirHangouts.prototype.onNewTabButtonClick = function(e)
     play_img2.addEventListener('click', this.onPlayAgainClick.bind(this), false);
 };
 
-BetterOnAirHangouts.prototype.showShinyBox = function(title, content, target)
-{
-    $(this).qtip(
-    {
+BetterOnAirHangouts.prototype.showShinyBox = function(title, content, target) {
+    $(this).qtip({
         id: 'boah_modal',
-        content:
-        {
+        content: {
             text: content,
-            title:
-            {
+            title: {
                 text: title,
                 button: true
             }
         },
-        position:
-        {
+        position: {
             my: 'top left',
             at: 'bottom left',
             target: target,
             adjust: { x: 0, y: 5 }
         },
-        show:
-        {
+        show: {
             event: false,
             solo: true,
             modal: false
@@ -101,19 +87,16 @@ BetterOnAirHangouts.prototype.showShinyBox = function(title, content, target)
     $(this).qtip('show');
 };
 
-BetterOnAirHangouts.prototype.getIframeUrl = function(button)
-{
+BetterOnAirHangouts.prototype.getIframeUrl = function(button) {
     var iframe_url = $(".boah_helper", this.findPost(button)).attr("iframe-url");
-    if (iframe_url.substr(0, 1) != 'h')
-    {
+    if (iframe_url.substr(0, 1) != 'h') {
         iframe_url = "http:" + iframe_url;
     }
 
     return iframe_url;
 };
 
-BetterOnAirHangouts.prototype.onEmbedButtonClick = function(e)
-{
+BetterOnAirHangouts.prototype.onEmbedButtonClick = function(e) {
     var iframe_url = this.getIframeUrl(e.target);
     iframe_url = iframe_url.replace('autoplay=1', 'autoplay=0');
     iframe_url = iframe_url.replace('autohide=1', 'autohide=0');
@@ -126,8 +109,7 @@ BetterOnAirHangouts.prototype.onEmbedButtonClick = function(e)
     this.showShinyBox('Hangout Embed Code', code_viewer, e.target);
 };
 
-BetterOnAirHangouts.prototype.onStreamButtonClick = function(e)
-{
+BetterOnAirHangouts.prototype.onStreamButtonClick = function(e) {
     var iframe_url = this.getIframeUrl(e.target);
     var stream_url = parseUri(iframe_url);
     stream_url = unescape(stream_url["queryKey"]["flvurl"]);
@@ -138,21 +120,16 @@ BetterOnAirHangouts.prototype.onStreamButtonClick = function(e)
     this.showShinyBox('Video Stream URL', code_viewer, e.target);
 };
 
-BetterOnAirHangouts.prototype.findPost = function(node)
-{
-    while (! $(node).hasClass("Te"))
-    {
+BetterOnAirHangouts.prototype.findPost = function(node) {
+    while (! $(node).hasClass("Te")) {
         node = node.parentNode;
     }
     return node;
 };
 
-BetterOnAirHangouts.prototype.createButtons = function(post)
-{
+BetterOnAirHangouts.prototype.createButtons = function(post) {
     var optionsDOM = post.querySelector('div.vo');
-    if (optionsDOM)
-    {
-        console.log("boobies")
+    if (optionsDOM) {
         optionsDOM.appendChild(document.createTextNode(' \u00a0-\u00a0 '));
 
         var helper_item = document.createElement('span');
@@ -173,7 +150,7 @@ BetterOnAirHangouts.prototype.createButtons = function(post)
         var option_new = document.createElement('img');
         option_new.setAttribute('src', 'chrome-extension://' + chrome.i18n.getMessage('@@extension_id') + '/img/new.png');
         option_new.setAttribute('class', 'c-C boah_menu_icon boah-menu-new');
-         $(option_new).hide();
+        $(option_new).hide();
         option_new.setAttribute('title', 'Open Hangout In New Tab');
         optionsDOM.appendChild(option_new);
         option_new.addEventListener('click', this.onNewTabButtonClick.bind(this), false);
@@ -183,7 +160,7 @@ BetterOnAirHangouts.prototype.createButtons = function(post)
         var option_embed = document.createElement('img');
         option_embed.setAttribute('src', 'chrome-extension://' + chrome.i18n.getMessage('@@extension_id') + '/img/embed.png');
         option_embed.setAttribute('class', 'c-C boah_menu_icon boah-menu-embed');
-         $(option_embed).hide();
+        $(option_embed).hide();
         option_embed.setAttribute('title', 'Get Hangout Embed Code');
         optionsDOM.appendChild(option_embed);
         option_embed.addEventListener('click', this.onEmbedButtonClick.bind(this), false);
@@ -193,15 +170,14 @@ BetterOnAirHangouts.prototype.createButtons = function(post)
         var option_stream = document.createElement('img');
         option_stream.setAttribute('src', 'chrome-extension://' + chrome.i18n.getMessage('@@extension_id') + '/img/stream.png');
         option_stream.setAttribute('class', 'c-C boah_menu_icon boah-menu-stream');
-         $(option_stream).hide();
+        $(option_stream).hide();
         option_stream.setAttribute('title', 'Get Hangout Stream URL');
         optionsDOM.appendChild(option_stream);
         option_stream.addEventListener('click', this.onStreamButtonClick.bind(this), false);
     }
 }
 
-BetterOnAirHangouts.prototype.onHangoutPostModified = function(e)
-{
+BetterOnAirHangouts.prototype.onHangoutPostModified = function(e) {
     if (e.target.nodeName != "IFRAME") { return; }
 
     var postDOM = this.findPost(e.target);
@@ -214,26 +190,22 @@ BetterOnAirHangouts.prototype.onHangoutPostModified = function(e)
 
 };
 
-BetterOnAirHangouts.prototype.renderAllItems = function(subtreeDOM)
-{
+BetterOnAirHangouts.prototype.renderAllItems = function(subtreeDOM) {
     var queryDOM = typeof subtreeDOM == 'undefined' ? document : subtreeDOM;
     var items = queryDOM.querySelectorAll('div[id^="update"]');
-    for (i in items)
-    {
+    for (i in items) {
         this.renderItem(items[i]);
     }
 };
 
 BetterOnAirHangouts.prototype.renderItem = function(itemDOM)
 {
-    if (!itemDOM || !itemDOM.parentNode || itemDOM.innerText == '')
-    {
+    if (!itemDOM || !itemDOM.parentNode || itemDOM.innerText == '') {
         return;
     }
 
     var hangoutDOM = itemDOM.querySelector("img." + HANGOUT_IMAGE_CLASS);
-    if (hangoutDOM)
-    {
+    if (hangoutDOM) {
         var postDOM = this.findPost(hangoutDOM)
         var postSpan = postDOM.querySelector("span.fD7nue");
         var postLink = postSpan.querySelector("a");
@@ -242,10 +214,8 @@ BetterOnAirHangouts.prototype.renderItem = function(itemDOM)
 
         this.createButtons(postDOM);
 
-        $.getJSON('http://betterhangouts.com/service/', {url:'plus.google.com/' + postLink.getAttribute("href"), from: "extension"}, function(data)
-        {
-            if (data.success == true)
-            {
+        $.getJSON('http://betterhangouts.com/service/', {url:'plus.google.com/' + postLink.getAttribute("href"), from: "extension"}, function(data) {
+            if (data.success == true) {
                 var iframe_url = data.embed;
                 iframe_url = iframe_url.replace('autoplay=0', 'autoplay=1');
                 iframe_url = iframe_url.replace('autohide=0', 'autohide=1');
@@ -255,9 +225,7 @@ BetterOnAirHangouts.prototype.renderItem = function(itemDOM)
                 $(".boah-menu-stream", $("#" + updateId)).show();
                 $(".boah-menu-embed", $("#" + updateId)).show();
             }
-            else
-            {
-                console.log("Better On Air Hangouts failed. #sadface")
+            else {
             }
         })
 
